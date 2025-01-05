517c7321fca01d17529841b2ddf3510c3c5fad30
Akmal Home <akmal.verint@gmail.com>
2025-01-05 20:12:26 +0500

    Start work on logging

--- src/main/java/uz/dadajon/backend/BackendApplication.java
+++ src/main/java/uz/dadajon/backend/BackendApplication.java
@@ -1,9 +1,13 @@
 package uz.dadajon.backend;
 
+import java.net.InetSocketAddress;
+import java.time.LocalTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Mono;
@@ -26,8 +30,10 @@ public class BackendApplication {
 	}
 
 	@GetMapping("/test")
-	public String test() {
-		return "Classic test endpoint exe";
+	public String test(ServerHttpRequest request) {
+		InetSocketAddress remoteAddress = request.getRemoteAddress();
+		String requestersIP = remoteAddress.getAddress().getHostAddress();
+		return requestersIP;
 	}
 
 	@GetMapping("/mono")
@@ -49,7 +55,11 @@ public class BackendApplication {
     // log file name: [date].log
     // log format: time requester-IP requested-MSISDN unique-ID-of-each-request/response status: requested, responded, error
 	@GetMapping("/command10")
-	public Mono<ResponseEntity<Object>> getMethodName(@RequestParam long msisdn) {
+	public Mono<ResponseEntity<Object>> getMethodName(@RequestParam long msisdn, ServerHttpRequest request) {
+		InetSocketAddress remoteAddress = request.getRemoteAddress();
+		String requestersIP = remoteAddress.getAddress().getHostAddress();
+		LocalTime requestTime = LocalTime.now();
+
 		return sshService.executeLaunchers(msisdn)
 			.map(ctailResponse -> ResponseEntity.ok().body((Object) ctailResponse))
 			.onErrorReturn(ResponseEntity.badRequest().body((Object) "no data"));
